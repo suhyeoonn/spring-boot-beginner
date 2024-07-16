8febdfc07311ba6b45c348d84d129a171f7094bf
suhyeoonn <suhyeon1740@gmail.com>
2024-07-17 04:13:49 +0900

    회원 목록 페이지 추가

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,8 +34,11 @@ public class MemberController {
 
         return "redirect:/";
     }
-//    @GetMapping("/members")
-//    public String members() {
-//        return "members";
-//    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
