a0fc5053d97ccae454dd1873339e3a37138fd9c2
suhyeoonn <suhyeon1740@gmail.com>
2024-07-24 15:00:09 +0900

    스프링 JdbcTemplate

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
@@ -26,6 +27,7 @@ public class SpringConfig {
     // 나중에 DB가 정해지면 여기만 변경하면 되는 이점이 있음
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
